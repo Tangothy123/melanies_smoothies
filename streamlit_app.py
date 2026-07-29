6f02822875223af54b22cc23694ec3e252bac64d
Tangothy123 <sam@sayesbury.com>
2026-07-29 17:43:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,6 @@ my_dataframe = session.table(
 )
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -42,6 +40,9 @@ if ingredients_list:
 
         ingredients_string += fruit_chosen + ' '
 
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + ' Nutrition Information')
 
         search_on = session.sql(f"""
