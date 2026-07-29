2f583095ee7532e640963544afce5d0bc9f63f02
Tangothy123 <sam@sayesbury.com>
2026-07-29 17:24:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,11 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered, ' + name_on_order + '!')
 
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+import requests
 
+smoothiefroot_response = requests.get(
+
+"https://my.smoothiefroot.com/api/fruit/watermelon"
+)
+
+st.text(smoothiefroot_response)
